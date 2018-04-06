7c1b3ec41fd430f840b76cea8635f95e8dde5b29
hrk794 <hrk794@gmail.com>
2018-04-06 19:25:23 +0530

    Adding solution to exercise 1-14.

---
+++ Chapter1/1-14/1-14a.c
@@ -0,0 +1,18 @@
+/*Write a program to print a histogram of the frequencies of dif-
+ferent characters in its input.*/
+
+
+#include <stdio.h>
+
+void main()
+
+{
+    int wordlength[26] = {0};
+    int c = 0;
+
+    while ((c = getchar()) != EOF) 
+    {
+        
+    }
+
+}
